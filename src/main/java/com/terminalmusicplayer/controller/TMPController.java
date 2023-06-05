0dcb8b20d5a83536a38acec8b2d8ede4c7dc30be
David-Sanma <97565612+David-Sanma@users.noreply.github.com>
2023-06-05 22:09:11 +0000

    Deploying to gh-pages from @ David-Sanma/Terminal-Music-Player@6316752d659a41af7b677d9d64a6b834b3c6389a 🚀

--- src/main/java/com/terminalmusicplayer/controller/TMPController.java
+++ src/main/java/com/terminalmusicplayer/controller/TMPController.java
@@ -1,14 +1,21 @@
 package com.terminalmusicplayer.controller;
+
 import com.terminalmusicplayer.models.Song;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.LineUnavailableException;
+import java.io.IOException;
+import javax.sound.sampled.AudioSystem;
 /**
  *
  * @author Sanma
  */
-public class TMPController {
+public class TMPController  {
     private Song song;
+    private String playSoundMessage;
     
-    public TMPController(String path, String format) { 
-        this.song = new Song(getFileNameWithPath(path), format);
+    public TMPController(String fileName, String format) { 
+        this.song = new Song(fileName, format);
     }
     
     public String getFileName() {
@@ -22,10 +29,35 @@ public class TMPController {
     public String songToString() {
         return this.song.toString();
     }
-    
-    private String getFileNameWithPath(String filePath) {
-        int lastSeparatorPosition = filePath.lastIndexOf("\\");
-        return filePath.substring(lastSeparatorPosition + 1, filePath.length());
+
+    public String getPlaySoundMessage() {
+        return this.playSoundMessage;
     }
+    
+    
+    public synchronized void playSoundThread(final AudioInputStream ais) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {  
+                    Clip clip = AudioSystem.getClip();
+                    clip.open(ais);
+                    clip.start();
+//                    for(int i = 0; i < 3; i ++) {
+//                        System.out.println("ADIOS");
+//                    }
+                } catch(IOException ioe) {
+                    System.out.println("ERROR: Input error.");
+                } catch(LineUnavailableException lue) {
+                    System.out.println("ERROR: Exception line.");
+                }   
+            }
+        }).start(); 
+    }
+    
+//    private String getFileNameWithPath(String filePath) {
+//        int lastSeparatorPosition = filePath.lastIndexOf("\\");
+//        return filePath.substring(lastSeparatorPosition + 1, filePath.length());
+//    }
     
 }
